2bce45dcecc99970cbfdeb999c0a0c2cb0b2185b
Orsi Kovacs <silla136@gmail.com>
2025-03-17 18:05:42 +0100

    user form with images

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :pets, dependent: :destroy
+  has_many :connections, dependent: :destroy
+  has_many :pets, through: :connections
+  has_one_attached :photo
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :age, presence: true, numericality: { greater_than: 16, only_integer: true }
@@ -14,8 +20,4 @@ class User < ApplicationRecord
 
   validates :location, presence: true, if: :provider?
   validates :about_me, presence: true, if: :provider?
-
-  has_many :pets, dependent: :destroy
-  has_many :connections, dependent: :destroy
-  has_many :pets, through: :connections
 end
