8aa168e39c8957c49cf00a2b0c95191c87042027
Christoph Alberter <christoph.alberter@googlemail.com>
2025-03-23 07:05:15 +0100

    landing page styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
-
   end
 
   def search
@@ -10,46 +9,47 @@ class PagesController < ApplicationController
     @pets = Pet.all
     @prompt = Prompt.new
 
-    if request.post?
-      @prompt = Prompt.new(prompt_params)
-      @prompt.user = current_user
+    return unless request.post?
 
-      # Get location and radius from params
-      location = params[:location]
-      radius = params[:radius].to_i
+    @prompt = Prompt.new(prompt_params)
+    @prompt.user = current_user
 
-      if location.present? && radius > 0
-        @pets_nearby = FindPetsService.call(location, radius)
-        @prompt.pets_for_prompt = @pets_nearby.to_json # PETS FOR PROMPT
+    # Get location and radius from params
+    location = params[:location]
+    radius = params[:radius].to_i
 
-        if @prompt.save
-          @output = @prompt.output # OUTPUT
-          Rails.logger.info("Prompt saved with output: " + @output) # for debuggin
-          ids = JSON.parse(@output) # Parse the IDs
-          @prompt.update(best_matches: ids) # Update the best_matches column with the IDs, to use later
+    if location.present? && radius > 0
+      @pets_nearby = FindPetsService.call(location, radius)
+      @prompt.pets_for_prompt = @pets_nearby.to_json # PETS FOR PROMPT
 
+      if @prompt.save
+        @output = @prompt.output # OUTPUT
+        Rails.logger.info("Prompt saved with output: " + @output) # for debuggin
+        ids = JSON.parse(@output) # Parse the IDs
+        @prompt.update(best_matches: ids) # Update the best_matches column with the IDs, to use later
 
-          @best_matches = ids.map { |id| Pet.find_by(id: id) }.compact # PETS ID MATCHING THE OUTPUT, mapped
+        @best_matches = ids.map { |id| Pet.find_by(id: id) }.compact # PETS ID MATCHING THE OUTPUT, mapped
 
-          respond_to do |format|
-            format.turbo_stream # IN CASE IT IS A TURBO REQUEST
-            format.html { redirect_to root_path } # IN CASE IT IS A REGULAR REQUEST (if we have not clicked submit yet for example)
+        respond_to do |format|
+          format.turbo_stream # IN CASE IT IS A TURBO REQUEST
+          # IN CASE IT IS A REGULAR REQUEST (if we have not clicked submit yet for example)
+          format.html do
+            redirect_to root_path
           end
-        else
-          flash.now[:alert] = "There was an error saving the data."
-          render :home, status: :unprocessable_entity
         end
       else
-        flash.now[:alert] = "Please provide both location and radius."
+        flash.now[:alert] = "There was an error saving the data."
         render :home, status: :unprocessable_entity
       end
+    else
+      flash.now[:alert] = "Please provide both location and radius."
+      render :home, status: :unprocessable_entity
     end
   end
 
   def other_matches
     @prompt = Prompt.find(params[:prompt_id])
     pet_ids = @prompt.best_matches
-
 
     @best_matches = pet_ids.map { |id| Pet.find_by(id: id) }.compact
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,13 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
     @pets = @user.pets
 
+    return unless @user.geocoded?
+
     @markers = [{
       lat: @user.latitude,
       lng: @user.longitude
-    }] if @user.geocoded?
+    }]
   end
 end
