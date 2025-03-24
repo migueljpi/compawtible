a01bf35704893dbfd4dbb2a1cc8943b22ac3a30f
Miguel Ines <miguel.joao.ines@gmail.com>
2025-03-24 15:10:54 +0000

    feat: other_matches front end ongoing

--- app/services/find_pets_service.rb
+++ app/services/find_pets_service.rb
@@ -1,7 +1,19 @@
 class FindPetsService
   def self.call(location, radius)
-    Pet.near(location, radius).map do |pet|
-      { id: pet.id, species: pet.species, breed: pet.breed, description: pet.description }
+    Rails.logger.info("FindPetsService called with location: #{location}, radius: #{radius}")
+
+    # Geocode the location to get latitude and longitude
+    coordinates = Geocoder.coordinates(location)
+    Rails.logger.info("Geocoded coordinates for location '#{location}': #{coordinates.inspect}")
+
+    if coordinates.present?
+      Rails.logger.info("Using radius: #{radius}")
+      Pet.near(coordinates, radius).map do |pet|
+        { id: pet.id, species: pet.species, breed: pet.breed, description: pet.description }
+      end
+    else
+      Rails.logger.warn("No coordinates found for location: #{location}")
+      []
     end
   end
 end
