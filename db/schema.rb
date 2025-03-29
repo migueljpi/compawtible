ac0f5510103bc6ebf085d1ace31de78dbbd23448
MauroSilva17 <m.henriquesilva18@gmail.com>
2025-03-29 14:13:33 +0100

    chore: db version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_03_23_075739) do
+ActiveRecord::Schema[7.2].define(version: 2025_03_23_150930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
