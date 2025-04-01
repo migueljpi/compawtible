cb3959670392d657e08848cde371e06ceaa5f1f3
Tanja Heinz <heinz.tanjamaria@gmail.com>
2025-04-01 21:42:56 +0200

    pundit favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,13 @@
 class FavoritesController < ApplicationController
   before_action :set_pet, only: %i[create destroy]
+  skip_after_action :verify_policy_scoped, only: [:create, :destroy]
 
   def create
+    # authorize @user
+    @favorite = Favorite.new
+    @favorite.favoritor = current_user
+    @favorite.favoritable = @pet
+    authorize @favorite
     current_user.favorite(@pet)
     respond_to do |format|
       format.turbo_stream
@@ -10,7 +16,11 @@ class FavoritesController < ApplicationController
   end
 
   def destroy
+    # authorize User  # This makes sure that the user is authorized to access the index
+
+
     favorite = current_user.favorites.find_by(favoritable: @pet)
+    authorize favorite
     if favorite
       current_user.unfavorite(@pet)
       respond_to do |format|
